91fb5582519047568d6a3279fe9aa5d61009892f
nkvdmax <84874356+nkvdmax@users.noreply.github.com>
2025-09-15 16:07:00 +0300

    fix: vercel deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const isVercel = !!process.env.VERCEL
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  base: '/anoma-intent-app-with-pages/',
+    base: isVercel ? '/' : '/anoma-intent-app-with-pages/',
+  }
 })
