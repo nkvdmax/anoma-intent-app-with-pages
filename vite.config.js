34c1dcd330ee2b25894b2746e3b5b85f7be53dec
nkvdmax <84874356+nkvdmax@users.noreply.github.com>
2025-09-15 18:46:47 +0300

    fix: conditional base for Vercel/GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import path from "node:path";
 
 export default defineConfig(() => ({
   plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  base: process.env.VERCEL ? "/" : "/anoma-intent-app-with-pages/",
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+  base: process.env.VERCEL === "1" ? "/" : "/anoma-intent-app-with-pages/",
 }));
